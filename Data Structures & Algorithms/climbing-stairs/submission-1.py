ccdd1a58e2f8cd057662e6cb1a5293af75fe1e48
Simon Tran <a3157542@adelaide.edu.au>
2026-09-22 16:02:25 +0930

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        ones, twos = 1, 2
+
+        for i in range(3, n + 1):
+            temp = ones + twos
+            ones = twos
+            twos = temp
+        return twos
